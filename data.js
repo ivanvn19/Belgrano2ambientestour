3a9f5135cd1763fb8363b09f282b725ef546aabc
ivanvn19 <ivan.vazquezneira@gmail.com>
2022-12-19 16:40:15 -0300

    tourbelgrano

---
+++ data.js
@@ -0,0 +1,508 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-vereda-",
+      "name": "Vereda ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -0.0003559800865744478,
+        "pitch": -0.8745855150182891,
+        "fov": 1.3108730938557924
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.4662124091821962,
+          "pitch": -0.011611822929998183,
+          "rotation": 0,
+          "target": "1-ingreso"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.1991253532248471,
+          "pitch": -0.19309230880437767,
+          "title": "2 Ambientes en el centro de San Miguel&nbsp;",
+          "text": "Amplio departamento 2 ambientes. Semipiso de 54m2&nbsp; cubiertos y 4 m2 descubiertos.&nbsp;<div>Al ingresar nos encontramos con&nbsp; la cocina con su respectivo bajo mesada y alacena, extractor de cocina.&nbsp;</div><div>Amplio living/comedor con salida al balcón. Dormitorio con placard y balcón con conexión para lavarropas.</div><div>Baño completo con bañera.</div><div>Cuenta con pisos de porcelanato dándole un lindo toque de elegancia a la unidad&nbsp;</div><div><br></div><div>para + info: www.castroestudioinmobiliario.com.ar&nbsp;</div><div>&nbsp;011 4451-3457</div><div>011 4093-9428</div><div>Sarmiento 1107- San miguel&nbsp; &nbsp; &nbsp; &nbsp;&nbsp;</div>"
+        }
+      ]
+    },
+    {
+      "id": "1-ingreso",
+      "name": "Ingreso",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0.8712594114347283,
+        "pitch": 0.02506258923572524,
+        "fov": 1.3108730938557924
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.2088332022806174,
+          "pitch": 0.18306845534239002,
+          "rotation": 0,
+          "target": "0-vereda-"
+        },
+        {
+          "yaw": 0.6640145008365685,
+          "pitch": 0.21813638816363223,
+          "rotation": 0,
+          "target": "2-hall"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-hall",
+      "name": "Hall",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.925461898800222,
+          "pitch": 0.16713512065088487,
+          "rotation": 0,
+          "target": "1-ingreso"
+        },
+        {
+          "yaw": -0.44034069944034826,
+          "pitch": 0.10200370739028841,
+          "rotation": 0,
+          "target": "3-ingreso-depto-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-ingreso-depto-",
+      "name": "Ingreso depto ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 2.4970513036649917,
+        "pitch": 0.040210828276702415,
+        "fov": 1.3108730938557924
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.8123245180862124,
+          "pitch": -0.04000963963583004,
+          "rotation": 0,
+          "target": "10-living-comedor-"
+        },
+        {
+          "yaw": 0.4490199838141695,
+          "pitch": -0.06964905694668033,
+          "rotation": 3.141592653589793,
+          "target": "2-hall"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-cocina-",
+      "name": "Cocina ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0.08192361795075875,
+        "pitch": 0.10555342422635583,
+        "fov": 1.3108730938557924
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.121895129435716,
+          "pitch": -0.021175726650598747,
+          "rotation": 0.7853981633974483,
+          "target": "6-pasillo-"
+        },
+        {
+          "yaw": 1.6395897741152616,
+          "pitch": 0.07260850496631832,
+          "rotation": 0,
+          "target": "10-living-comedor-"
+        },
+        {
+          "yaw": 2.1454363422034994,
+          "pitch": 0.04892573125995092,
+          "rotation": 1.5707963267948966,
+          "target": "3-ingreso-depto-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-balcn-frente-",
+      "name": "Balcón Frente ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -0.8973881317480732,
+        "pitch": 0.08631704180072575,
+        "fov": 1.3108730938557924
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.9383707223217392,
+          "pitch": 0.18048670584630422,
+          "rotation": 0,
+          "target": "10-living-comedor-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-pasillo-",
+      "name": "Pasillo ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0.651884177539646,
+        "pitch": 0.16838284340869691,
+        "fov": 1.3108730938557924
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.6175417901217646,
+          "pitch": 0.10024949699521457,
+          "rotation": 4.71238898038469,
+          "target": "4-cocina-"
+        },
+        {
+          "yaw": 1.5962843568652296,
+          "pitch": 0.02984884469365845,
+          "rotation": 0,
+          "target": "7-bao"
+        },
+        {
+          "yaw": -0.0989041693078363,
+          "pitch": 0.0696950711032045,
+          "rotation": 0,
+          "target": "8-habitacin-"
+        },
+        {
+          "yaw": 2.971181697120647,
+          "pitch": 0.054320623380235133,
+          "rotation": 0,
+          "target": "10-living-comedor-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-bao",
+      "name": "Baño",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0.02116520055138338,
+        "pitch": 0.4164033153601725,
+        "fov": 1.3108730938557924
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.1336685534382482,
+          "pitch": 0.26987240195310136,
+          "rotation": 0.7853981633974483,
+          "target": "6-pasillo-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-habitacin-",
+      "name": "Habitación ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -0.800418399498966,
+        "pitch": 0.08343082885448538,
+        "fov": 1.3108730938557924
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0385263787319765,
+          "pitch": 0.17436004328464172,
+          "rotation": 0,
+          "target": "6-pasillo-"
+        },
+        {
+          "yaw": -0.30354288915304295,
+          "pitch": 0.0472020702556577,
+          "rotation": 0,
+          "target": "9-balcn-contrafrente-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-balcn-contrafrente-",
+      "name": "Balcón contrafrente ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -0.2546755949338646,
+        "pitch": -0.01507906060377806,
+        "fov": 1.3108730938557924
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.7340844099425974,
+          "pitch": 0.16810797146483836,
+          "rotation": 0,
+          "target": "8-habitacin-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-living-comedor-",
+      "name": "Living comedor ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0.7962237706732189,
+        "pitch": 0.17175074344734398,
+        "fov": 1.3108730938557924
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5385950266630326,
+          "pitch": 0.09017141293985986,
+          "rotation": 0,
+          "target": "5-balcn-frente-"
+        },
+        {
+          "yaw": -1.8247012610211026,
+          "pitch": 0.037961417411768394,
+          "rotation": 7.853981633974483,
+          "target": "4-cocina-"
+        },
+        {
+          "yaw": -2.0110000421573275,
+          "pitch": 0.022193943832974128,
+          "rotation": 0,
+          "target": "6-pasillo-"
+        },
+        {
+          "yaw": -2.354478234917055,
+          "pitch": 0.07104027604438201,
+          "rotation": 4.71238898038469,
+          "target": "3-ingreso-depto-"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Belgrano al 400",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": true
+  }
+};
